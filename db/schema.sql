482caa4636650409ce769f26b572473d7bafee07
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-03 12:25:56 -0500

    added schema for mySQL database;

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS astrology;
+
+CREATE DATABASE IF NOT EXISTS astrology;
+
+USE astrology;
+
+CREATE TABLE emails (
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(100) NOT NULL,
+  PRIMARY KEY(id)
+)
