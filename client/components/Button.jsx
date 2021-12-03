a85654eb11d0f00299f3e9836ffe6b46e626c9f4
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-03 13:51:08 -0500

    moved all components into components folder for better file structure

---
+++ client/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Button = ({ text, handleOnClick }) => {
+  return (
+    <button
+      className={`${text}-button`}
+      onClick={() => {
+        handleOnClick();
+      }}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
