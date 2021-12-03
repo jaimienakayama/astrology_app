ce5f3af30d02f7d8df97610a1c282a4f6a26c5a9
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-03 12:31:17 -0500

    added response logic

--- client/ResponseField.jsx
+++ client/ResponseField.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "./Button.jsx";
 
-const ResponseField = ({
-  sign,
-  toggleView,
-  dateRange,
-  color,
-  compatibility,
-  currentDate,
-  description,
-  luckyNumber,
-  luckyTime,
-  mood,
-}) => {
+const ResponseField = ({ sign, toggleView, horoscope, reset }) => {
+  const [dateRange, setDateRange] = useState("");
+  const [color, setColor] = useState("");
+  const [compatibility, setCompatibility] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [luckyNumber, setLuckyNumber] = useState("");
+  const [luckyTime, setLuckyTime] = useState("");
+  const [mood, setMood] = useState("");
+
+  useEffect(() => {
+    if (horoscope) {
+      setDateRange(horoscope.date_range);
+      setColor(horoscope.color);
+      setCompatibility(horoscope.compatibility);
+      setCurrentDate(horoscope.current_date);
+      setDescription(horoscope.description);
+      setLuckyNumber(horoscope.lucky_number);
+      setLuckyTime(horoscope.luckyTime);
+      setMood(horoscope.mood);
+    }
+  }, [horoscope]);
+
   const handleOnClick = () => {
+    reset();
     toggleView();
   };
 
@@ -32,7 +44,7 @@ const ResponseField = ({
           Daily horoscope: {description}
         </div>
       </div>
-      <Button text="back" handleOnClick={toggleView} />
+      <Button text="back" handleOnClick={handleOnClick} />
     </>
   );
 };
