590acbe22ba528e446d29c3e5ee063307d83803a
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-03 12:29:18 -0500

    refactored code to seperate concerns and added Footer componenet to app

--- client/App.jsx
+++ client/App.jsx
@@ -1,36 +1,21 @@
 import ReactDOM from "react-dom";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import SignField from "./SignField.jsx";
 import ResponseField from "./ResponseField.jsx";
+import Footer from "./Footer.jsx";
 
 const App = () => {
   const [responseShown, setResponseShown] = useState(false);
   const [sign, setSign] = useState("");
   const [horoscope, setHoroscope] = useState("");
-  const [dateRange, setDateRange] = useState("");
-  const [color, setColor] = useState("");
-  const [compatibility, setCompatibility] = useState("");
-  const [currentDate, setCurrentDate] = useState("");
-  const [description, setDescription] = useState("");
-  const [luckyNumber, setLuckyNumber] = useState("");
-  const [luckyTime, setLuckyTime] = useState("");
-  const [mood, setMood] = useState("");
-
-  useEffect(() => {
-    if (horoscope) {
-      setDateRange(horoscope.date_range);
-      setColor(horoscope.color);
-      setCompatibility(horoscope.compatibility);
-      setCurrentDate(horoscope.current_date);
-      setDescription(horoscope.description);
-      setLuckyNumber(horoscope.lucky_number);
-      setLuckyTime(horoscope.luckyTime);
-      setMood(horoscope.mood);
-    }
-  }, [horoscope]);
 
   const toggleView = () => {
     setResponseShown(!responseShown);
+  };
+
+  const reset = () => {
+    setSign("");
+    setHoroscope("");
   };
 
   return (
@@ -46,16 +31,11 @@ const App = () => {
         <ResponseField
           sign={sign}
           toggleView={toggleView}
-          dateRange={dateRange}
-          color={color}
-          compatibility={compatibility}
-          currentDate={currentDate}
-          description={description}
-          luckyNumber={luckyNumber}
-          luckyTime={luckyTime}
-          mood={mood}
+          horoscope={horoscope}
+          reset={reset}
         />
       )}
+      <Footer />
     </>
   );
 };
