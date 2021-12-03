016cbcd30a0cf92088505f474b98a9efe52eac5f
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-03 00:17:38 -0500

    uimplemented response component to show response from api request

--- client/ResponseField.jsx
+++ client/ResponseField.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import Button from "./Button.jsx";
 
-const ResponseField = ({ sign, handleOnClick }) => {
+const ResponseField = ({
+  sign,
+  toggleView,
+  dateRange,
+  color,
+  compatibility,
+  currentDate,
+  description,
+  luckyNumber,
+  luckyTime,
+  mood,
+}) => {
+  const handleOnClick = () => {
+    toggleView();
+  };
+
   return (
     <>
       <div className="response-header">
         <h3>{sign}</h3>
-        <div className="response-field"></div>
+        <div className="response-field">
+          Current date: {currentDate}
+          Date range: {dateRange}
+          Compatibility: {compatibility}
+          Color: {color}
+          Lucky number: {luckyNumber}
+          Lucky time: {luckyTime}
+          Mood: {mood}
+          Daily horoscope: {description}
+        </div>
       </div>
-      <Button text="back" handleOnClick={handleOnClick} />
+      <Button text="back" handleOnClick={toggleView} />
     </>
   );
 };
