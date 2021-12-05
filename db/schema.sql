1c560fb40cc757923239a2cdec608562838f2067
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-04 23:05:52 -0500

    refactored schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,3 @@
-DROP DATABASE IF EXISTS astrology;
-
-CREATE DATABASE IF NOT EXISTS astrology;
-
-USE astrology;
-
 CREATE TABLE emails (
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(100) NOT NULL,
